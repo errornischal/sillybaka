1e4b8641739814e01cd8d4d56aa3bc7b6558974b
errornischal <103837665+errornischal@users.noreply.github.com>
2022-04-21 08:28:46 +0545

    Add files via upload

--- error.js
+++ error.js
@@ -5,3 +5,31 @@ menu.onclick = () => {
     navigationmenu.classList.toggle('active');
     menu.classList.toggle('move');
 }
+
+
+var swiper = new Swiper(".trend-cont", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay:{
+        delay:5000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1068: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
