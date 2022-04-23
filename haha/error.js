1efc30dc9c62cf20692bb53f856d3b43c61aef30
errornischal <103837665+errornischal@users.noreply.github.com>
2022-04-23 13:02:55 +0545

    Delete haha directory

--- haha/error.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('.menu-icon');
-let navigationmenu = document.querySelector('.menu');
-
-menu.onclick = () => {
-    navigationmenu.classList.toggle('active');
-    menu.classList.toggle('move');
-}
